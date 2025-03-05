cebe30daacd5ecc8b0bc8a12e6445771a6bee046
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2025-03-05 17:07:08 -0600

    NeoTrinkey HID program
    touch #1 to "shoot' (send stream of " " spaces)
    touch #2 to go autopilot mode/stop autopilot
    #1 and #2 to end program

---
+++ AutoPilot.py
@@ -0,0 +1,97 @@
+import time
+import usb_hid
+import random
+import board
+import neopixel
+import touchio
+
+touch1 = touchio.TouchIn(board.TOUCH1)
+touch2 = touchio.TouchIn(board.TOUCH2)
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
+
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, auto_write=True)
+
+
+#Define colors
+pink = (12,10,12)
+gold = (50, 40, 5)
+blue = (0,0,8)
+orange = (25, 10, 0)
+blank = (0,0,0)
+grn = (0,20,0)
+green  = (0,20,0)
+red = (20,0,0)
+zero = (2,2,2)
+colors = [pink, gold, blue, orange,green,red]
+
+
+moves = [Keycode.DOWN_ARROW,Keycode.UP_ARROW,Keycode.LEFT_ARROW,Keycode.RIGHT_ARROW]
+REPL = False
+
+
+def shoot():
+    pixels.fill(blue)
+    for x in range(1+random.randrange(4)):
+        keyboard_layout.write(" ")
+    time.sleep(.1)
+    pixels.fill(blank)
+
+def jiggle():
+    steps = 3 + random.randrange(5)
+    for i in range(steps):
+        pixels.fill(random.choice(colors))
+        dir = moves[random.randrange(4)]
+        for x in range(10):
+            keyboard.send(dir)
+    pixels.fill(blank)
+
+C3P0 = False
+Done = False
+
+
+while not Done:
+    Val = 0
+    if touch1.value:
+        Val = Val + 1
+    if touch2.value:
+        Val = Val + 2
+    if Val == 1 :
+        print("shoot")
+        shoot()
+
+    if Val == 2 :
+        if C3P0:
+            print("droid deactivated")
+            C3P0 = False
+            pixels.fill(red)
+            time.sleep(2)
+
+
+        else :
+            print("droid engaged")
+            C3P0 = True
+            pixels.fill(green)
+            time.sleep(.5)
+
+    if Val == 3 :
+        Done = True
+    if C3P0:
+        for r in range(random.randrange(5)):
+            shoot()
+            time.sleep(.25)
+            jiggle()
+            time.sleep(.25)
+
+    time.sleep(.1)
+print("End of the Line")
+pixels.fill(gold)
+time.sleep(5)
+
+
